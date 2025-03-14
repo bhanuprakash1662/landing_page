e1d8e8187afb09e84f75c26ff4fa4c4958c4da74
bhanuprakash1662 <kandibhanuprakash@gmail.com>
2025-03-14 13:12:31 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,17 +6,24 @@ const Hero = () => {
         <header id="home" className="hero">
             <div className="hero-content">
                 <h1>Pioneering the Future of Artificial Intelligence and Digital Innovation</h1>
-                <p>[7:02 pm, 12/3/2025] Kandi Bhanu Prakash: Ra
-[7:03 pm, 12/3/2025] Kandi Bhanu Prakash: Institute lo unna
-[7:03 pm, 12/3/2025] Narshimha KR: Okay ra
-[7:40 pm, 12/3/2025] Narshimha KR: Aipoyaka call chey ra
-[8:08 pm, 12/3/2025] Narshimha KR: lensuptechnologies@gmail.com 
+                <p>At LensUp Technologies, we revolutionize the digital era with cutting-edge Al, ML, Computer Vision, AR, and VR solutions. From intelligent automation and real-time image processing to immersive virtual experiences, we lead innovation, empowering businesses and creators with future-ready, scalable, and secure digital products that redefine user engagement in the evolving tech landscape.
 
-Password: Teddy@14323
-[12:38 pm, 14/3/2025] Kandi Bhanu Prakash: https://docs.google.com/forms/d/1h-bkbavrJyl_X8RmSNVJ78Air4yTfJtel1GIhtzj9xE/edit#responses
-[12:39 pm, 14/3/2025] Kandi Bhanu Prakash: ..
-[12:40 pm, 14/3/2025] Kandi Bhanu Prakash: https://landing-page-seven-beige.vercel.app/
-[1:09 pm, 14/3/2025] Narshimha KR: At LensUp Technologies, we revolutionize the digital era with cutting-edge Al, ML, Computer Vision, AR, and VR solutions. From intelligent automation and real-time image processing to immersive virtual experiences, we lead innovation, empowering businesses and creators with future-ready, scalable, and secure digital products that redefine user engagement in the evolving tech landscape.</p>
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </p>
             </div>
         </header>
     );
