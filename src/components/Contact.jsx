1ce09865ec13d578bb6cbb274472c77c2bb553ef
bhanuprakash1662 <kandibhanuprakash@gmail.com>
2025-03-14 13:32:55 +0530

    new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
                 <h2>Contact Us</h2>
                 <p><strong>Company Name:</strong> LensUp Technologies Pvt. Ltd.</p>
                 <p><strong>Email:</strong>lensuptechnologies@gmail.com </p>
-                <p><strong>Phone:</strong>6304-116759</p>
+                <p><strong>Phone:</strong>6304-116753</p>
                 {/* <p><strong>Address:</strong> 123 AI Street, Tech City, TX 75001</p>
                 <p><strong>Working Hours:</strong> Mon - Fri: 9 AM - 6 PM</p> */}
             </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import './Hero.css';
+import lenss from '../assests/lensss.png'
 
 const Hero = () => {
     return (
         <header id="home" className="hero">
             <div className="hero-content">
+            <img src={lenss} alt="Pioneering Innovation" className="hero-image" />
                 <h1>Pioneering the Future of Artificial Intelligence and Digital Innovation</h1>
                 <p>At LensUp Technologies, we revolutionize the digital era with cutting-edge Al, ML, Computer Vision, AR, and VR solutions. From intelligent automation and real-time image processing to immersive virtual experiences, we lead innovation, empowering businesses and creators with future-ready, scalable, and secure digital products that redefine user engagement in the evolving tech landscape.
-
-                    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                 </p>
             </div>
         </header>
